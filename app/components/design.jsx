a7817c6a1d3a19b43cd83a28e95697a3d68f0f73
Ricardo Vazquez <ricardo@mozillafoundation.org>
2015-07-21 11:32:43 -0400

    Adding Google Drive links.
    Fixes https://github.com/MozillaFoundation/Design/issues/57

--- app/components/design.jsx
+++ app/components/design.jsx
@@ -7,41 +7,38 @@ var Design = React.createClass({
         <div className="header">
           <h2>Design</h2>
         </div>
-        <div className="header alert">
-          <h2>This page is still under construction. Links will not work.</h2>
-        </div>
         <div className="design-intro">
           <header>
             <h2>Welcome to the Design Hub of the Mozilla Foundation.</h2>
           </header>
         </div>
         <div className="main-fluid design">
-          <h3 className="description">Whether you’re looking to learn more about how Mozilla Foundation designers work or quickly trying to find a logo, we’ve got everything you need in this central place. If you can’t find what you’re looking for, <a href="#">let us know</a>.</h3>
+          <h3 className="description">Whether you’re looking to learn more about how Mozilla Foundation designers work or quickly trying to find a logo, we’ve got everything you need in this central place. If you can’t find what you’re looking for, <a href="mailto:mofodesign@mozilla.org">let us know</a>.</h3>
           <div id="assets">
             <h2>Resources</h2>
             <div className="grid-4">
               <h4>Principles</h4>
-              <p>The <a href="#" className="link">principles</a> we care about building into our products, our process and our team.
+              <p>The <a href="https://github.com/MozillaFoundation/Design/issues/39" target="_" className="link">principles</a> we care about building into our products, our process and our team.
               </p>
-              <a href="#" className="button">Design Principles</a>
+              <a href="https://github.com/MozillaFoundation/Design/issues/39" target="_" className="button">Design Principles</a>
             </div>
             <div className="grid-4">
               <h4>Templates & Guidelines</h4>
-              <p>Standards and kits for incoporating new work into existing projects, including documentation for <a href="#" className="link">Webmaker</a>, <a href="#" className="link">Mozilla Learning Networks</a> and <a href="#" className="link">Mozilla Advocacy</a>.
+              <p>Standards and kits for incoporating new work into existing projects, including documentation for <a href="https://drive.google.com/a/mozillafoundation.org/file/d/0ByIoeeW0a3R_TmpZelpYSnRnQzA/view" target="_" className="link">Webmaker</a>, <a href=" https://drive.google.com/a/mozilla.com/folderview?id=0B4Q8pzCpDS_eR2dpNnJXRTR1UVU&usp=sharing" target="_"className="link">Mozilla Learning Networks</a> and Mozilla Advocacy.
               </p>
-              <a href="#" className="button">Templates & Guides</a>
+              <a href="https://drive.google.com/open?id=0ByIoeeW0a3R_UjEzdDU4NlMzYXc" target="_" className="button">Templates & Guides</a>
             </div>
             <div className="grid-4">
               <h4>Assets</h4>
-              <p><a href="#" className="link">Logos</a>, <a href="#" className="link">fonts</a>, <a href="#" className="link">colours</a>, <a href="#" className="link">UX & UI templates</a> for mobile, tablet and desktop, <a href="#" className="link">photography</a>, <a href="#" className="link">illustration</a>, etc.
+              <p><a href="https://drive.google.com/open?id=0B_rbDAen9prkZjRsMDJMa3h0WUE" target="_" className="link">Logos</a>, <a href="https://drive.google.com/open?id=0B_rbDAen9prkYkFhOTNBX2RpYUU" target="_" className="link">fonts</a>, <a href="https://drive.google.com/open?id=0B_rbDAen9prkUmd1N1lFM3E0dFk" target="_" className="link">colours</a>, <a href="https://www.flickr.com/photos/mozilladrumbeat/favorites/" target="_" className="link">photography</a>, <a href="https://drive.google.com/open?id=0BzfN9sXlOXt1RmE5dlhpQ2JCVXc" target="_" className="link">illustration</a>, etc.
               </p>
-              <a href="#" className="button">Assets Folder</a>
+              <a href="https://drive.google.com/open?id=0ByIoeeW0a3R_STBRYnYzTTJvVkk" target="_" className="button">Assets Folder</a>
             </div>
             <div className="grid-4">
               <h4>Process</h4>
-              <p>More about <a href="#" className="link">how we work</a>, including how we relish working with <a href="#" className="link">design contributors</a>.
+              <p>More about <a href="https://github.com/xmatthewx/Mofo-Design-Handbook/tree/master/Process" target="_" className="link">how we work</a>, including how we relish working with <a href="https://github.com/MozillaFoundation/Design/issues/50" target="_" className="link">design contributors</a>.
               </p>
-              <a href="#" className="button">Tools & Process</a>
+              <a href="https://github.com/MozillaFoundation/Design/issues/43" target="_" className="button">Tools & Process</a>
             </div>
           </div>
         </div>
